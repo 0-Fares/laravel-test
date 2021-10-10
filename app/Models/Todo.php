561a2f6e7d1e5e9206e0bbdf2865608837f0284f
Mustapha Fares OMAR AMRANI <MustaphaFares.OMAR.algerie@viacesi.fr>
2021-10-10 18:00:38 +0100

    +Relationships

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Todo extends Model
 {
@@ -28,4 +29,10 @@ class Todo extends Model
         'complete' => 'boolean',
         'user_id' => 'int'
     ];
+
+    // Model relationship
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
